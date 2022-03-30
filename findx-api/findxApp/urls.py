4c505e67d0325deda00aeb9d6fd23d8686ec3052
mwiks_dev <mwikali119@gmail.com>
2022-03-30 23:48:38 +0300

    Add obtain token path

--- findx-api/findxApp/urls.py
+++ findx-api/findxApp/urls.py
@@ -5,9 +5,11 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from .views import *
 
 router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('account/register', views.UserCreate.as_view())
+    path('api-auth/', ObtainAuthToken.as_view()),
 
 ]
--- findx-api/findxProject/urls.py
+++ findx-api/findxProject/urls.py
@@ -1,16 +1,8 @@
-from django.urls import include, path
-from rest_framework import routers
-from findxApp import views
-from rest_framework.authtoken.views import obtain_auth_token 
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'), 
+    path('admin/', admin.site.urls),
+    path('',include('findxApp.urls'))
 ]
