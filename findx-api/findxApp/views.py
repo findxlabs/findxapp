f9173cff52d351898c42cfc3d5da71c1a5ab8d7d
morehwachege <antony123muriithi@gmail.com>
2022-04-13 19:38:37 +0300

    user route calls

--- findx-api/findxApp/views.py
+++ findx-api/findxApp/views.py
@@ -1,12 +1,12 @@
 # from django.shortcuts import render
 
-from . import UserSerializer
+from .serializers import UserSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
 
 
-class UserSerializer(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
 
     permission_classes = (IsAuthenticated,)
     serializer_class = UserSerializer
--- findx-api/findxProject/urls.py
+++ findx-api/findxProject/urls.py
@@ -20,7 +20,9 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
- 
+
+from rest_framework.routers import DefaultRouter
+from findxApp.views import UserViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+router = DefaultRouter()
+router.register('user', UserViewSet, basename='user')
+urlpatterns += router.urls
