d6c1997fdb064caca379b310d531f5ca4c042b3e
Vitalis <vitalismaina18@gmail.com>
2023-11-01 10:13:44 +0300

    [refactor] generate rsa keys

--- src/app/app_init/page.js
+++ src/app/app_init/page.js
@@ -1,20 +1,38 @@
-import React from 'react';
+const crypto = require('crypto');
+const { v4: uuidv4 } = require('uuid');
 
-const Init = () => {
-    const jsonData = {
-        "data": {
-            "publicKey": process.env.PUBLIC_KEY,
-            "defaultLanguage": process.env.DEFAULT_LANGUAGE || "EN",
-            "minAndroidAppVersion": process.env.MIN_ANDROID_APP_VERSION || "",
-            "minIOSAppVersion": process.env.MIN_IOS_APP_VERSION || "",
-            "appInitSignature": "random_uuid"
+export default function Init() {
+    // Generate RSA key pair
+    const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem'
         },
-        "statusCode": 200,
-        "message": "Success",
-        "succeeded": true
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem'
+        }
+    });
+
+    // Store the private key securely (you can use a secure storage mechanism)
+    // For example, you can use environment variables, secure vaults, or databases.
+
+    // Generate random UUID
+    const appInitSignature = uuidv4();
+
+    const jsonData = {
+        data: {
+            publicKey: publicKey, // Return the public key in the response
+            defaultLanguage: process.env.DEFAULT_LANGUAGE || 'EN',
+            minAndroidAppVersion: process.env.MIN_ANDROID_APP_VERSION || '',
+            minIOSAppVersion: process.env.MIN_IOS_APP_VERSION || '',
+            appInitSignature: appInitSignature // Use the generated UUID
+        },
+        statusCode: 200,
+        message: 'Success',
+        succeeded: true
     };
 
     return <pre>{JSON.stringify(jsonData, null, 2)}</pre>;
-};
-
-export default Init;
+}
