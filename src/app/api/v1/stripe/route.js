dbade7d6c7196d05437b43063e445185f9b1fd42
Vitalis <vitalismaina18@gmail.com>
2023-11-06 10:33:26 +0300

    [refactor] include logic for stripe integration

---
+++ src/app/api/v1/stripe/route.js
@@ -0,0 +1,24 @@
+import { NextResponse, NextRequest } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  typescript: true,
+  apiVersion: "2022-11-15",
+});
+
+export async function POST(req) {
+  const { data } = await req.json();
+  const { amount } = data;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Number(amount) * 100,
+      currency: "USD",
+    });
+
+    return new NextResponse(paymentIntent.client_secret, { status: 200 });
+  } catch (error) {
+    return new NextResponse(error, {
+      status: 400,
+    });
+  }
+}
