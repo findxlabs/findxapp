0af5d9c9e86be83a94122ac7a8ab5e1aa5319cb8
Vitalis <vitalismaina18@gmail.com>
2023-11-06 11:52:00 +0300

    [refator] add logic for stripe

--- src/app/api/v1/stripe/route.js
+++ src/app/api/v1/stripe/route.js
@@ -1,10 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  typescript: true,
-  apiVersion: "2022-11-15",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 export async function POST(req) {
   const { data } = await req.json();
