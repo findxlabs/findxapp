1361fd44e21226d881b389bc935ea65d3044d45c
Vitalis <vitalismaina18@gmail.com>
2023-11-06 11:53:54 +0300

    [code cleanup] remove used import

--- src/app/api/v1/stripe/route.js
+++ src/app/api/v1/stripe/route.js
@@ -1,4 +1,4 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse, } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
